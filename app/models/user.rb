07edde1bb3b06984d0a1c773b3983bd71625ef18
sakaiyuji3862 <sakaiya0526@icloud.com>
2021-01-03 22:31:18 +0900

    ログイン・ログアウト機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         has_many :tweets
+
+         validates :nickname, presence: true
+         validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze} #半角英数字
+         validates :password, confirmation: true
 end
