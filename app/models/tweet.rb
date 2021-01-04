d49ffdcbd34acf5c6ae14e5730ea1131688c7147
sakaiyuji3862 <sakaiya0526@icloud.com>
2021-01-04 16:14:43 +0900

    tweet テーブル　バリデーション

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,21 @@
 class Tweet < ApplicationRecord
+    has_one_attached :image
+    belongs_to :User
+
+
+    with_options presence: true do
+        validates :name
+        validates :price, format: {with: /\A[0-9]+\z/}, #半角数字
+                  numericality: {
+                    greater_than_or_equal_to: 300,
+                    less_than_or_equal_to: 9999999
+                  }
+        validates :text
+        validates :shipping_cost_id, numericality: { other_than: 1 }
+        validates :shipping_day_id, numericality: { other_than: 1 }
+        validates :prefecture_id, numericality: { other_than: 1 }
+        validates :image
+    end
+
+
 end
