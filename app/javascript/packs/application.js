e78bea6b9321124ce292f6315a7843556d2e8be2
sakaiyuji3862 <sakaiya0526@icloud.com>
2021-01-11 00:21:16 +0900

    入力された販売価格によって、販売手数料や販売利益の表示が変わるように

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@ require("@rails/ujs").start()
 // require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("../mobilemenu.js")
+require("../mobile_menu.js")
+require("../item_price.js")
 //= require jquery
 //= require jquery_ujs
 
