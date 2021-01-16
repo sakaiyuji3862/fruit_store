7997f59818b1f6523491299173c404873da06b07
sakaiyuji3862 <sakaiya0526@icloud.com>
2021-01-16 16:20:13 +0900

    formオブジェクト作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,27 @@
 class OrdersController < ApplicationController
+    before_action :set_find, only: [:index, :create]
     def index
-        @tweet = Tweet.find(params[:tweet_id])
+        @order_address = OrderAddress.new
     end
+
+    def create
+        @order_address = OrderAddress.new(order_address_params)
+        if @order_address.valid?
+            @order_address.save
+            redirect_to root_path
+        else
+            render action: :index
+        end
+    end
+end
+
+private
+
+def order_address_params
+    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :address, :building_name,
+                                          :phone_number).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
+end
+
+def set_find
+    @tweet = Tweet.find(params[:tweet_id])
 end
---
+++ app/models/order_address.rb
@@ -0,0 +1,21 @@
+class OrderAddress
+    include ActiveModel::Model
+    attr_accessor :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :tweet_id
+
+    with_options presence: true do
+        validates :post_code, format: {with: /\A\d{3}[-]\d{4}\z/} # 郵便番号（ハイフンあり7桁）
+        validates :city
+        validates :address
+        validates :phone_number, format: {with: /\A\d{10,11}\z/} # 携帯番号(ハイフンなし10桁or11桁)
+        validates :prefecture_id, numericality: {other_than: 1}
+        validates :user_id
+        validates :tweet_id
+        # validates :token
+    end
+
+    def save
+        order = Order.create(user_id: user_id, tweet_id: tweet_id)
+        Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address,
+                       building_name: building_name, phone_number: phone_number, order_id: order.id)
+    end
+end
