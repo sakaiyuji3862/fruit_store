ff464c027b7cd287eefab4d39866fc5583e4231b
sakaiyuji3862 <sakaiya0526@icloud.com>
2021-01-20 01:41:26 +0900

    エラーメッセージ日本語化　修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -18,4 +18,5 @@ class OrderAddress
         Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address,
                        building_name: building_name, phone_number: phone_number, order_id: order.id)
     end
+
 end
