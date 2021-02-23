f65ba3c25b39fdc80ef805879821c1f35c8edfbc
sakaiyuji3862 <sakaiya0526@icloud.com>
2021-02-23 20:57:17 +0900

    ゲストログイン 実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,11 @@ class User < ApplicationRecord
             validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze} #半角英数字
             validates :password, confirmation: true
          end
+
+   def self.guest
+      find_or_create_by!(email: 'test@test.com') do |user|
+         user.password = SecureRandom.alphanumeric(10) + [*'a'..'z'].sample(1).join + [*'0'..'9'].sample(1).join
+         user.nickname = 'ゲストユーザー'
+      end
+   end
 end
